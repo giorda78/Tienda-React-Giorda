ef756e758b4615e6917d06a0b32b61ce7a21572b
giorda78 <lucasgiorda78@gmail.com>
2021-09-30 14:54:10 -0300

    desafio clase 4

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
 
         <ul class="nav justify-content-center bg-dark m-3 p-4">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="#">Inicio</a>
             </li>
 
             <li class="nav-item">
